9e4fbd758b42edbe47a8e295f7e0721ac2fc5d46
Vanderlei Chagas <vanderleichagas42@gmail.com>
2022-01-20 17:29:26 -0300

    feat(pages): adjust breakpoint

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -5,14 +5,15 @@ export const GridContainer = styled.div`
     grid-template-columns: auto auto auto auto;
     padding: 10px;
     grid-gap: 20px;
-    @media only screen and (max-width: 70rem){
+
+    @media only screen and (max-width: 85rem){
         grid-template-columns: auto auto auto;
     }
     @media only screen and (max-width: 65rem){
         grid-template-columns: auto auto;
     }
     @media only screen and (max-width: 45rem){
-        grid-template-columns: auto ;
+        grid-template-columns: auto;   
     }
     
     margin-top: 40px;
